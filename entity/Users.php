0187b1ace8c6b36a57681d2021d07c849b2b9b7a
DarkToL <botinok30@gmail.com>
2024-01-26 19:04:32 +0400

    new pro

---
+++ entity/Users.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace app\entity;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $login
+ * @property string $password
+ * @property int $is_admin
+ *
+ * @property Orders[] $orders
+ * @property UserProfile[] $userProfiles
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['login', 'password'], 'required'],
+            [['is_admin'], 'integer'],
+            [['login'], 'string', 'max' => 50],
+            [['password'], 'string', 'max' => 100],
+            [['login'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'login' => 'Login',
+            'password' => 'Password',
+            'is_admin' => 'Is Admin',
+        ];
+    }
+
+    /**
+     * Gets query for [[Orders]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrders()
+    {
+        return $this->hasMany(Orders::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[UserProfiles]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserProfiles()
+    {
+        return $this->hasMany(UserProfile::class, ['user_id' => 'id']);
+    }
+}
---
+++ migrations/m231212_142151_BD_user_basis.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\db\Migration;
+
+class m231212_142151_BD_user_basis extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('users', [
+            'id' => $this->primaryKey(),
+            'login' => $this->string(50)->unique()->notNull(),
+            'password' => $this->string(100)->notNull(),
+            'is_admin' => $this->boolean()->defaultValue(false)->notNull(),
+        ]);
+
+        $this->createTable('user_profile', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'fio' => $this->string(100)->notNull(),
+            'phone' => $this->integer()->notNull(),
+            'address' => $this->string(100)->notNull(),
+            'email' => $this->string(100)->notNull(),
+        ]);
+        $this->addForeignKey(
+            'users_to_user_profile_fk',
+            'user_profile',
+            'user_id',
+            'users',
+            'id',
+            'CASCADE',
+            'CASCADE',
+        );
+
+        $this->createTable('orders', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'date' => $this->timestamp()->defaultExpression('NOW()')->notNull(),
+            'price' => $this->integer()->notNull(),
+        ]);
+        $this->addForeignKey(
+            'users_to_orders_fk',
+            'orders',
+            'user_id',
+            'users',
+            'id',
+            'CASCADE',
+            'CASCADE',
+        );
+
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('users');
+        $this->dropTable('user_profile');
+        $this->dropTable('orders');
+    }
+
+}
